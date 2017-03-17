3d24aaccddff12e85f93020f66a1ef9732c50e67
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-03-17 13:23:29 -0500

    Renaming test class

--- tests/Feature/PeopleParserTest.php
+++ tests/Feature/PeopleParserTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class PeopleParserTest extends TestCase
 {
     use DatabaseMigrations, DatabaseTransactions;
 
